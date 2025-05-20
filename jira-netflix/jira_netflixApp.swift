fcd4b3d26c93ac0bb4de404c766f0bea355e9ab4
Melih Buğra Esmek <32802959+Mbesmek@users.noreply.github.com>
2025-05-20 19:55:21 +0300

    Initial Commit

---
+++ jira-netflix/jira_netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  jira_netflixApp.swift
+//  jira-netflix
+//
+//  Created by Melih Buğra Esmek on 20.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct jira_netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
